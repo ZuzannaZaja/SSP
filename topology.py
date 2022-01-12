8efa58796ed3f715cd79e88311f4eba73ac92929
Maciej Jankowski <m.jankowski0@gmail.com>
2022-01-12 23:31:22 +0100

    move switch numbers up by one to get nice dpids

--- topology.py
+++ topology.py
@@ -8,12 +8,12 @@ class FamtarTopo(Topo):
 
         leftHost = self.addHost('h1')
         rightHost = self.addHost('h2')
-        switches = [self.addSwitch('s_{}'.format(i)) for i in range(number_of_switches)]
+        switches = [self.addSwitch('s_{}'.format(i)) for i in range(1, number_of_switches+1)]
 
         # TODO: add link speeds here?
-        switch_links = [(0, 1), (0, 5), (0, 6), (1, 2), (1, 6), (2, 3), (4, 3), (5, 4), (5, 6), (6, 2), (6, 4), (6, 3)] 
+        switch_links = [(1, 2), (1, 6), (1, 7), (2, 3), (2, 7), (3, 4), (5, 4), (6, 5), (6, 7), (7, 3), (7, 5), (7, 4)]
         for left, right in switch_links:
-            self.addLink(switches[left], switches[right])
+            self.addLink(switches[left-1], switches[right-1])
 
         self.addLink(leftHost, switches[0])
         self.addLink(rightHost, switches[3])
